0e744fc27db2b30a14aa2215dbe9701f1dd119fa
Bruno Costa Candia <bruno852812@gmail.com>
2022-09-04 17:44:07 -0300

    add electron files

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { resolve } = require('path')
 const { app, Menu, Tray } = require('electron');
 
 app.on('ready', () => {
-  const tray = new Tray(resolve(__dirname, 'assets', 'tray-icon.png'));
+  const tray = new Tray(resolve(__dirname, 'assets', 'iconTemplate.png'));
 
   const contextMenu = Menu.buildFromTemplate([
     {
